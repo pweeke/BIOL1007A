01fec9b27ffddac8479b80b9bdcc91d72415c911
Penelope Weekes <penelopeweekes@penelopes-mbp.middlebury.edu>
2023-01-20 13:19:34 -0500

    fix push error git status

--- 2023_01_19_Matricies_DataFrames.R
+++ 2023_01_19_Matricies_DataFrames.R
@@ -54,10 +54,84 @@ orderedIris <- iris[order(iris$Petal.Length),] ##order iris by petal.length cols
 head(orderedIris)
 
 
+#### FUNCTIONS in R!!
 
+# everything in R is a function
+sum(3,2) #sum
+3 + 2 # + sign is a function
+sd
 
+### user-defined functions
 
+## functionName <- function(argX=defaultX, argY=defaultY){}
+  #curly bracket starts the body of the function
+  ### lines of code and notes
+  ### create local variables (only visible to R within the function)
+  ### return(z) - instead of print for within a function
 
+myFunc <- function(a=3, b=4){
+  z <- a + b
+  return(z)
+}
+
+myFunc() ##runs default- or z
+myFunc(a=100, b=3.4) #outputs the changed arguments
+
+myFuncBad <- function(a=3) {
+  z <- a + b
+  return(z)
+}
+myFuncBad() #doesn't work 
+
+# DOESN'T recognize variables within function until saved outside as well
+
+### multiple return statements
+
+################################################################################
+# FUNCTION: HWE
+# input: all allele freq p (0,1)
+# output: p and the freq of 3 genotypes AA AB BB
+#------------------------------------------------------------------------------
+#^ you can hide that as a section
+
+HWE <- function(p=runif(1)){
+  if(p>1.0 | p<0.0){
+    return("Function Failure: p must be bweteen 0 and 1")
+  }
+  q <- 1-p
+  fAA <- p^2
+  fAB <- 2*p*q
+  fBB <- q^2
+  vecOut <- signif(c(p = p, AA = fAA, AB = fAB, BB = fBB), digits = 3)
+    return(vecOut)
+}
+#############################################################################
+
+HWE()
+freqs <- HWE()
+freqs
+HWE(p=3)
+
+### create a complex default value
+####################################################################
+#FUNCTION: fitlinear2
+# fits simple regression line
+# input: list (p) of predictor (x) and response (y)
+# outputs: slope and p-value
+
+fitlinear2 <- function(p=NULL){
+  if(is.null(p)){
+    p <- list(x=runif(20), y=runif(20))
+  }
+  myMod <- lm(p$x~p$y)
+  myOut <- c(slope=summary(myMod)$coefficients[2,1], pValue= summary(myMod)$coefficients[2,4])
+  plot(x=p$x, y=p$y) #quick plot to check output
+  return(myOut)
+}
+
+fitlinear2() #default that simulated for us
+myPars <- list(x=1:10, y=runif(10))
+fitlinear2(p=myPars)
 
 
 
