bbc6b9fd17b15690bb15da1a95281fe824033943
Penelope Weekes <penelopeweekes@penelopes-mbp.middlebury.edu>
2023-01-26 14:15:27 -0500

    create assignment 8

---
+++ 2023_01_26_Loading_in_Data.R
@@ -0,0 +1,32 @@
+#### Lecture 8: Loading in Data
+#### 26 January 2023
+### PPW
+
+### Create and save a dataset
+### write.table(x=varName, file="outputFileName.csv", header=TRUE, sep=",")
+
+
+### These functions read in a data set
+### read.table(file="path/to/data.csv", header=TRUE, sep =",")
+### read.cs(file="data.csv", header=T)
+
+### Use RDS objects when only working in R
+# helpful in large datasets
+## saveRDS(my_data, file="FileName.RDS")
+## readRDS("FileName.RDS")
+## p <- readRDS("FileName.RDS")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
